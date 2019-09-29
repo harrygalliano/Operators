a3f009669fa40dbfc842805ba2368498824d1dd7
Harry Galliano <harrygalliano97@gmail.com>
2019-09-29 11:21:10 +0100

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,111 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("previousResult =  " + previousResult);
+        result = result - 1;    // 3 - 1 = 2
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previousResult =  " + previousResult);
+
+        result = result * 10;   // 2 * 10 = 20
+        System.out.println("2 * 10 = " + result);
+
+        result = result / 5;    // 20 / 5 = 4
+        System.out.println("20 / 5 = " + result);
+
+        result = result % 3;    // the remainder of (4 % 3) = 1
+        System.out.println("4 % 3 = " + result);
+
+        // result = result + 1;
+        result++; // 1 + 1 = 2
+        System.out.println("1 + 1 = " + result);
+
+        result--;  // 2 - 1 = 1
+        System.out.println("2 - 1 = " + result);
+
+        // result = result +2;
+        result += 2;  // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+
+        // result = result * 10;
+        result *= 10; // 3 * 10 = 30
+        System.out.println("3 * 10 = " + result);
+
+        // result = result / 3
+        result /= 3;  // 30 / 3 = 10
+        System.out.println("30 / 3 = " + result);
+
+        // result = result - 2
+        result -= 2;  // 10 - 2 = 8
+        System.out.println("10 - 2 = " + result);
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if (isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
+
+
+        /* Operator challenge
+        lets test what we have learned about operators.
+        1. Create a double variable with a value of 20.0
+        2. Create a second variable of type double with the value of 80.0
+        3. Add both numbers together and multiple by 100.00
+        4. Use the remainder operator to figure out what the remainder
+        from the result of the operation in step 3 and 40.00.
+        5. Create a boolean variable that assigns value true if the remainder
+        for #4 is 0, or false if its not 0.
+        6. output the boolean variable.
+        7. write an if-then statement that displays a message "Got some remainder"
+        if the boolean in step 5 is not true.
+         */
+
+        double firstDouble = 20.00d;
+        double secondDouble = 80.00d;
+        double calcDouble = (firstDouble + secondDouble) * 100.00d;
+        double remainderDouble = calcDouble % 40.00d;
+        boolean isNoRemainder = (remainderDouble == 0) ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+        if (!isNoRemainder) {
+            System.out.println("got some remainder");
+        }
+
+
+
+    }
+}
